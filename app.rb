ec6c71e5db58cd040605fddf26f41790a7941656
Meenoow <100647073+Meenoow@users.noreply.github.com>
2023-09-27 16:26:05 +0000

    random number, square root and square pages working

--- app.rb
+++ app.rb
@@ -6,16 +6,60 @@ get("/") do
   erb(:square)
 end
 
+#Square form and results
 get("/square/new") do
-  @number = 3
-  result = @number * @number
-
-  "<p>The answer is #{result}</p>"
-
+  
   erb(:square)
 end
 
 get("/square/results") do
- 
+  @the_num = params.fetch("user_number").to_f
+  
+  @the_result = @the_num ** 2
+
   erb(:square_results)
+end
+
+#Square root form and results
+get("/square_root/new") do
+  
+  erb(:square_root)
+end
+
+get("/square_root/results") do
+  @the_num = params.fetch("user_number").to_f
+  
+  @the_result = @the_num ** 0.5
+
+  
+  erb(:square_root_results)
+end
+
+#Payment with form
+get("/payment/new") do
+  
+  erb(:payment)
+end
+
+get("/payment/results") do
+  #@the_num = params.fetch("user_number").to_f
+  
+  #@the_result = @the_num ** 0.5
+
+  
+  erb(:payment_results)
+end
+
+#Random
+get("/random/new") do
+  
+  erb(:random)
+end
+
+get("/random/results") do
+  @min = params.fetch("user_number_min").to_f
+  @max = params.fetch("user_number_max").to_f
+  @random = rand(@min..@max).to_f
+  
+  erb(:random_results)
 end
